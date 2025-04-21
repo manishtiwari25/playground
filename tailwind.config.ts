e3b53afc70e6174708d2a9209a983241e857a02b
manishtiwari25 <manish.ertiwari@outlook.com>
2025-04-21 09:56:30 +0200

    chore: updated versions

---
+++ tailwind.config.ts
@@ -0,0 +1,19 @@
+import type { Config } from "tailwindcss";
+
+import { nextui } from "@nextui-org/theme";
+
+/** @type {import('tailwindcss').Config} */
+const config: Config = {
+  content: [
+    "./components/**/*.{js,ts,jsx,tsx,mdx}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx}",
+    "./node_modules/@nextui-org/theme/dist/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {},
+  },
+  darkMode: "class",
+  plugins: [nextui()],
+};
+
+export default config;
